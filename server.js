abe838ba456355b92b883e796cef930ed9aa0d8d
natemallison <healthynate@gmail.com>
2020-12-01 08:53:17 -0500

    Testing redirect to subdomain.

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = require('express')
-const path = require('path')
-const serveStatic = require('serve-static')
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
 
-let app = express()
-app.use(serveStatic(path.join(__dirname, '/dist')))
+let app = express();
+app.use(serveStatic(path.join(__dirname, "/dist")));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
+app.use(function forceLiveDomain(req, res, next) {
+  // Don't allow user to hit Heroku now that we have a domain
+  var host = req.get("Host");
+  if (host === "natethedev-votingappfrontend.herokuapp.com/") {
+    return res.redirect(301, "https://votingapp.natethedev.com");
+  }
+  return next();
+});
 
-const port = process.env.PORT || 8080
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log('Listening on port ' + port)
-})
+  console.log("Listening on port " + port);
+});
